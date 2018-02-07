102aa2a9b218bcf8fd73a2e4e8a027c364f528a5
Chris Chow <chris.yo.chow@gmail.com>
2018-02-07 18:46:09 +0800

    Initial Commit

---
+++ MyOwnChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyOwnChatApp
+//
+//  Created by Chris Chow on 7/2/2018.
+//  Copyright © 2018 Yau On Chow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
