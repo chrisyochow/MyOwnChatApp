7f536c649ab827bcb2cc0110bf90b3534212d450
Chris Chow <chris.yo.chow@gmail.com>
2018-02-07 23:46:33 +0800

    Configure SWRevealController

---
+++ MyOwnChatApp/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  MyOwnChatApp
+//
+//  Created by Chris Chow on 7/2/2018.
+//  Copyright © 2018 Yau On Chow. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 50
+    }
+
+}
