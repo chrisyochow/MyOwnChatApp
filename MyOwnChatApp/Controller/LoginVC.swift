71017c7f51540dd94b870bf238fb48781f9e9f7c
Chris Chow <chris.yo.chow@gmail.com>
2018-02-08 21:49:02 +0800

    Login VC created

---
+++ MyOwnChatApp/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  MyOwnChatApp
+//
+//  Created by Chris Chow on 8/2/2018.
+//  Copyright © 2018 Yau On Chow. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
