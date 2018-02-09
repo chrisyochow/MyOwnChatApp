c4945286f6f49cf608da4d83b2e2115a11546891
Chris Chow <chris.yo.chow@gmail.com>
2018-02-09 14:04:24 +0800

    Register User Function done

--- MyOwnChatApp/Controller/CreateAccountVC.swift
+++ MyOwnChatApp/Controller/CreateAccountVC.swift
@@ -11,6 +11,11 @@ import UIKit
 @IBDesignable
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +25,28 @@ class CreateAccountVC: UIViewController {
     
     @IBAction func closeBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    
+    @IBAction func createBtnPassed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else { return }
+        guard let password = passwordTxt.text, passwordTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
+    }
+    
+    
+    @IBAction func pickAvatarPassed(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func pickBGColorPassed(_ sender: Any) {
+        
     }
     
 }
--- MyOwnChatApp/Utility/Constants.swift
+++ MyOwnChatApp/Utility/Constants.swift
@@ -8,7 +8,20 @@
 
 import Foundation
 
+//Handler
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL
+let BASE_URL = "https://mychitchatapp.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
